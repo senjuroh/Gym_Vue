141ed2991926a13a65b071093f96d9c5b7627d32
Rohan <senjurohan7@gmail.com>
2022-12-21 14:50:00 +0545

    fonts assigned

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,15 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;700&family=Rubik+Spray+Paint&display=swap");
+:root {
+  --primary: #1a548d;
+  --secondary: #05b2e2;
+}
+.head1 {
+  font-family: "Rubik Spray Paint", cursive;
+}
+p {
+  font-family: "Open Sans", sans-serif;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Vuesax from "vuesax";
 
-Vue.config.productionTip = false
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+Vue.use(Vuesax, {
+  // options here
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
